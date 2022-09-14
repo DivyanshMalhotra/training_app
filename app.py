0d891b9f1e7e4dede790e752cdccf0f28011195d
Divyansh Malhotra <divyansh_malhotra@mckinsey.com>
2022-09-14 10:19:35 +0530

    CI Commit

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-    return "<h2> This is the first web app by Divyansh Malhotra</h2>"
+    return "<h2> This is the first web app by Divyansh Malhotra with CI</h2>"
 if __name__ == "__main__":
     app.run(host = "0.0.0.0")
